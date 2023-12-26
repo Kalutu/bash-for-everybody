6017e5c6f4bd91d6421fdb3f06aca6815c6beab5
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-26 16:03:24 +0300

    Added a newline before displaying the fortune

--- Fortune/fortune.sh
+++ Fortune/fortune.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+# Fortune Teller
 
+echo -e "\n~~ Fortune Teller ~~\n"
+
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+
+GET_FORTUNE() {
+    if [[ ! $1 ]]; then
+        echo 'Ask a YES or NO question'
+    else
+        echo 'Try again. Make sure it ends with a question mark'
+    fi
+    read -p "Your question: " QUESTION
+}
+
+GET_FORTUNE
+until [[ $QUESTION =~ \?$ ]]; do
+    GET_FORTUNE again
+done
+
+echo -e "\nThe fortune-teller says: ${RESPONSES[$N]}"
